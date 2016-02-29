cbfbc89db0b1ccfcd9d06920669927988b8de8bd
IIgorLima <igor.lima.8201@gmail.com>
2016-02-28 22:09:19 -0300

    Create main.c
    this function shows how to return an output to the User

---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>    //It includes all library functions "stdio.h" in your code, including the "printf ()"
+
+void main(void)    //first function called when the program runs
+{
+  printf("Hello world!\n");   //function that returns a output for example "Hello world!"
+}
